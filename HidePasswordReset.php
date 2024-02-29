8bf02aeb844f5aa600e4bb7351115b93366eff70
jvelletti <jvelletti@allplan.com>
2024-02-29 15:09:41 +0100

    set Version to 5.0.1, Add automatic conversion of external link to reset password and allow Matomo V5.x

--- HidePasswordReset.php
+++ HidePasswordReset.php
@@ -10,22 +10,39 @@ namespace Piwik\Plugins\HidePasswordReset;
 
 class HidePasswordReset extends \Piwik\Plugin
 {
-	public function registerEvents()
+    public function registerEvents()
     {
         return array(
             'Template.loginNav' => 'commentOutElement',
-			'Template.loginNav' => 'commentOutElement',
+            'Template.loginNav' => 'commentOutElement',
         );
     }
-	
-	public function commentOutElement(&$outString,$area){
-		if($area === "top"){
-			$outString = "<!--";
-		}else if($area === "bottom"){
-			$outString = "-->";
-			
-			$settings = new SystemSettings();
-			$outString .= \Piwik\Common::sanitizeInputValues($settings->message->getValue());
-		}
-	}
+
+    public function commentOutElement(&$outString,$area){
+        if($area === "top"){
+            $outString = "<!--";
+        }else if($area === "bottom"){
+            $outString = "-->";
+
+            $settings = new SystemSettings();
+            $string = \Piwik\Common::sanitizeInputValues($settings->message->getValue());
+            if ( strlen( $string ) < 4 ) {
+                $outString .= $string ;
+            } else {
+                $stringExplode = explode(" " , $string ) ;
+                foreach ( $stringExplode as $part ) {
+                    if(substr( $part , 0 , 8) == "https://") {
+                        $outString .= '<a href="' . $part . '" target="_blank">' . $part . "</a> " ;
+                    } else {
+                        $outString .= $part . ' ' ;
+                    }
+                }
+                unset($stringExplode);
+                unset($part);
+            }
+            unset($string) ;
+
+
+        }
+    }
 }
--- docs/index.md
+++ docs/index.md
@@ -3,3 +3,7 @@
 The easiest way to install is to find the plugin in the [Matomo Marketplace](https://plugins.matomo.org/).
 Just Activate on the plugin page after install and the "Lost your password?" message will be removed.
 A replacement login message may be supplied on the General Settings screen (see screenshots).
+
+new in Version 5.0:
+if the replacement login message contains "https://", the URL is automatically converted to a link 
+
